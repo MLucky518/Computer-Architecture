6ced2a318a8220b5c72ee9ec317ad8ffbe7babe9
MLucky518 <56007160+MLucky518@users.noreply.github.com>
2020-09-13 17:51:53 -0400

    day 4 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,31 +9,25 @@ class CPU:
     def __init__(self):
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.sp = 7  # Last index in the registry is 7
+        self.reg[self.sp] = 0xF4  # reserved space for the start of stack
         self.running = False
         self.pc = 0
+        self.branch_table = {
+
+            0b01000111: self.op_PRN,
+            0b10000010: self.op_LDI,
+            0b10100010: self.op_MUL,
+            0b00000001: self.op_HLT,
+            0b01000101: self.push_to_stack,
+            0b01000110: self.pop_from_stack,
+            0b01010000: self.call,
+            0b00010001: self.return_op,
+            0b10100000: self.ADD
+        }
 
     def load(self):
         """Load a program into memory."""
-        self.read_argv()
-        # address = 0
-
-        # # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
-    def read_argv(self):
         params = sys.argv
 
         if len(params) != 2:
@@ -49,22 +43,66 @@ class CPU:
                         num = split_comment[0].strip()
                         if num == '':
                             continue
-                        num2 = int("0b"+num,2)
-                        self.ram_write(address,num2)
+                        num2 = int("0b"+num, 2)
+                        self.ram_write(address, num2)
                         address += 1
             except:
                 print("file not found")
                 sys.exit(2)
-                            
 
+    def push_to_stack(self, a, b):
+        # decrements the stack counter by one as the stack grows downward
+        self.reg[self.sp] -= 1
 
+        reg_val = self.reg[a]
+        self.ram_write(self.reg[self.sp], reg_val)
+        self.pc += 2
 
-    def alu(self, op, reg_a, reg_b):
+    def pop_from_stack(self, a, b):
+        popped_val = self.ram_read(self.reg[self.sp])
+        self.reg[a] = popped_val
+        self.reg[self.sp] += 1
+        self.pc += 2
+
+    def op_MUL(self, a, b):
+        self.alu("MUL", a, b)
+
+    def return_op(self, a, b):
+        self.pc = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
+
+    def call(self, a, b):
+        return_address = self.pc + 2
+        self.reg[self.sp] -= 1
+        self.ram_write(self.reg[self.sp], return_address)
+        reg_num = self.ram[self.pc+1]
+        self.pc = self.reg[reg_num]
+
+    def op_HLT(self, a, b):
+        self.running = False
+
+    def ADD(self, a, b):
+        self.alu("ADD", a, b)
+
+    def op_PRN(self, a, b):
+        print(self.reg[a])
+        self.pc += 2
+
+    def op_LDI(self, a, b):
+
+        self.reg[a] = b
+        self.pc += 3
+
+    def alu(self, op, a, b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.reg[a] += self.reg[b]
+            self.pc += 3
         # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[a] = self.reg[a] * self.reg[b]
+            self.pc += 3
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -95,31 +133,18 @@ class CPU:
         self.ram[mar] = mdr
 
     def run(self):
+
         self.running = True
-        HLT = 0b00000001
-        PRN = 0b01000111
-        LDI = 0b10000010  # binary code for 130
-        MUL = 0b10100010
         self.load()
-        self.trace()
+
         while self.running:
-            current_instruction = self.ram_read(self.pc)
             op_a = self.ram_read(self.pc+1)
             op_b = self.ram_read(self.pc+2)
+            current_instruction = self.ram_read(self.pc)
 
-            if current_instruction == LDI:
-                self.reg[op_a] = op_b
-                self.pc += 3
+            self.branch_table[current_instruction](op_a, op_b)
 
-            elif current_instruction == PRN:
-                print(self.reg[op_a])
-                self.pc += 2
-            elif current_instruction == MUL:
-                self.reg[op_a] = self.reg[op_a] * self.reg[op_b]
-                self.pc+=3
 
-            elif current_instruction == HLT:
-                self.running = False
 cpu = CPU()
 
 
